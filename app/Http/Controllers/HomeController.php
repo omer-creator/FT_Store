de9f6eba101ebc9c07bf109bf6e9fc49d60b71ae
Omer Afzal <afzalmuhammadomer@gmail.com>
2022-12-15 18:57:43 +0500

    get usertype data automatically from the database

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\User;
+
 class HomeController extends Controller
 {
-    //
+    public function redirect()
+    {
+        $usertype=Auth::user()->usertype;
+    }
 }
