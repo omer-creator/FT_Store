19967058e62935ad8384e0007f0f9a89fb942b9c
Omer Afzal <afzalmuhammadomer@gmail.com>
2022-12-15 17:53:28 +0500

    Added phone, address fields in register page

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('usertype')->default(0);
             $table->string('phone')->nullable();
-            $table->string('address')->nullable(0);
+            $table->string('address')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
