99791f09169b9fbc14bf853cd54b2c0bedbe93d3
Omer Afzal <afzalmuhammadomer@gmail.com>
2022-12-16 04:20:04 +0500

    Integrated adminpanel in laravel project

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,16 +1,29 @@
-<x-app-layout>
-
-</x-app-layout>
-
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Admin Dashboard</h1>
-</body>
+  <head>
+    <!-- Required meta tags -->
+    @include('admin.css')
+
+  </head>
+  <body>
+    <div class="container-scroller">
+
+      <!-- partial:partials/_sidebar.html -->
+      @include('admin.sidebar')
+      <!-- partial -->
+
+      <div class="container-fluid page-body-wrapper">
+
+        <!-- partial:partials/_navbar.html -->
+        @include('admin.header')
+        <!-- partial -->
+        @include('admin.body')
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    <script src="admin/assets/vendors/js/vendor.bundle.base.js"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    @include('admin.script')
+    <!-- End custom js for this page -->
+  </body>
 </html>
